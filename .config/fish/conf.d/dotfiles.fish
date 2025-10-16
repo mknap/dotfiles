83faebba02c39b7942841c1ea6322071afba2006
Mike <michael.j.knap@gmail.com>
2025-10-16 16:39:20 -0500

    added fish

---
+++ .config/fish/conf.d/dotfiles.fish
@@ -0,0 +1,4 @@
+# Using git and this alias to manage dotfiles
+# https://mjones44.medium.com/storing-dotfiles-in-a-git-repository-53f765c0005d
+
+alias config="git --git-dir=$HOME/dotfiles --work-tree=$HOME"
